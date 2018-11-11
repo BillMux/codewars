578d39b1b87c5500a08b3461446b809ca4c42459
Max <maxprovin01@gmail.com>
2018-11-11 22:20:52 +0000

    we have a capital

--- jadenCase.js
+++ jadenCase.js
@@ -3,12 +3,12 @@
 String.prototype.toJadenCase = function() {
   var words = this.split(' ');
   var x = 0;
-  // console.log(words.charAt(0).toUpperCase());
+
   while (x < words.length) {
-    console.log(words.charAt(0).toUpperCase());
+    var currentWord = words[x];
+    var capital = currentWord.charAt(0).toUpperCase();
     x++;
   };
-
-  // console.log(words)
 };
+
 "How can mirrors be real if our eyes aren't real".toJadenCase()
