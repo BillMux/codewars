cef7ee8f0f537964555e2bd395117bbe63de2d5d
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-11 19:58:36 +0000

    set vars for vowels and letters in string

--- vowelCount.js
+++ vowelCount.js
@@ -1,8 +1,12 @@
-function getCount(str) {
+// split str into array of chars DONE
+// for each letter, check if vowel
+// if it is, increase vowelCount
+
+function getCount(string) {
   var vowelsCount = 0;
-
-  // enter your majic here
-
+  var vowelsArr = ['a','e','i','o','u'];
+  var stringArray = string.split('');
+  
 
 
   return vowelsCount;
