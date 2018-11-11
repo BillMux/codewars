9f21a9a1f1a3c5baf11ab560baac6caaba468242
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-11 22:44:36 +0000

    foreach

--- jadenCase.js
+++ jadenCase.js
@@ -3,12 +3,15 @@
 String.prototype.toJadenCase = function() {
   var words = this.split(' ');
   var x = 0;
-
-  while (x < words.length) {
-    var currentWord = words[x];
+  var newString = ''
+  words.forEach(function(currentWord) {
     var capital = currentWord.charAt(0).toUpperCase();
+    var wordWithoutFirst = currentWord.substr(1);
+    var newWords = capital + wordWithoutFirst
+    newString += newWords + ' '
     x++;
-  };
+  });
+  return newString.substr(0, newString.length-1)
 };
 
 "How can mirrors be real if our eyes aren't real".toJadenCase()
