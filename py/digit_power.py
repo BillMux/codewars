0d85a55479507edbe9983db5b145b73bd5622dc1
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-25 18:59:47 +0000

    Completes digit power kata

--- py/digit_power.py
+++ py/digit_power.py
@@ -1,10 +1,3 @@
-# take the args subject and power
-# subject is split into digits
-# first digit is powered by power
-# second digit is powered by power + 1 etc...
-# return the number
-
-
 def digit_power(subject, power):
     count = 0
     int_str_list = map(int, str(subject))
@@ -13,8 +6,3 @@ def digit_power(subject, power):
         count += int_str_list[i] ** (power + i)
 
     return count / subject if(count % subject == 0) else -1
-
-
-print(digit_power(89, 1), digit_power(89, 1) == 1)
-print(digit_power(92, 1), digit_power(92, 1) == -1)
-print(digit_power(46288, 3), digit_power(46288, 3) == 51)
