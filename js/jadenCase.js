26dc6b102e26b2dc9308837ce416091ebff29b5d
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-25 21:30:58 +0000

    Remove charAt function

--- js/jadenCase.js
+++ js/jadenCase.js
@@ -2,8 +2,8 @@
 // capitalize them
 String.prototype.toJadenCase = function() {
   return this.split(' ').map(function(word) {
-    return word.charAt(0).toUpperCase() + word.substr(1)
+    return word[0].toUpperCase() + word.substr(1)
   }).join(' ');
 };
 
-"How can mirrors be real if our eyes aren't real".toJadenCase()
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase())
