19bcc98559fba641bf53c0fd026839dab1e8935e
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-28 14:27:15 +0000

    Continue to next iteration if no arr found

--- js/nestingStructure.js
+++ js/nestingStructure.js
@@ -1,10 +1,18 @@
+var x = 0;
+
 Array.prototype.sameStructureAs = function (other) {
   for (var i = 0; i < this.length; i++) {
     if ((Array.isArray(this[i])) && (Array.isArray(other[i]))) {
+      console.log(`Level ${x}: both are arrays`)
+      x++;
       this[i].sameStructureAs(other[i]);
     } else if ((Array.isArray(this[i])) !== (Array.isArray(other[i]))) {
+      console.log(`Level ${x}: one is array`)
       return false;
-    } else { return this.length === other.length };
+    } else {
+      console.log(`Level ${x}: none are arrays`)
+      continue;
+    };
   };
   return true;
 };
@@ -12,17 +20,17 @@ Array.prototype.sameStructureAs = function (other) {
 console.log([ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] ),
            ![ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] )); // false
 
-console.log([ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ),
-            [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ) === true);
+// console.log([ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ),
+//             [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ) === true);
 
-console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ),
-            [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ) === true);
-
-console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] ),
-           ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] )); // false
-
-console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] ),
-           ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] )); // false
-
-console.log([ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ),
-            [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] )); // true
+// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ),
+//             [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ) === true);
+//
+// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] ),
+//            ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] )); // false
+//
+// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] ),
+//            ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] )); // false
+//
+// console.log([ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ),
+//             [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] )); // true
