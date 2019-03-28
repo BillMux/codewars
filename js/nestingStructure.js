d8d05c67feab8ce649c81779cb73f665f7a90378
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-28 15:13:54 +0000

    Add brief doc comment

--- js/nestingStructure.js
+++ js/nestingStructure.js
@@ -1,14 +1,14 @@
+// This function determines whether two arrays have the same nesting structures
+
 Array.prototype.sameStructureAs = function (other) {
-  if (Array.isArray(this) !== Array.isArray(other)) { return false };
+  if (!Array.isArray(other)) { return false };
   for (var i = 0; i < this.length; i++) {
     if ((Array.isArray(this[i])) && (Array.isArray(other[i]))) {
       if (this[i].length !== other[i].length) { return false };
       return this[i].sameStructureAs(other[i]);
     } else if ((Array.isArray(this[i])) !== (Array.isArray(other[i]))) {
       return false;
-    } else {
-      continue;
-    };
+    } else { continue; };
   };
   return true;
 };
