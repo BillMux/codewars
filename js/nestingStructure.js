9b34d2b3aa865e00b77d8f242a35b025e0c0ecb5
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-28 14:42:29 +0000

    Challenge complete, function equates nesting structures

--- js/nestingStructure.js
+++ js/nestingStructure.js
@@ -1,36 +1,14 @@
-var x = 0;
-
 Array.prototype.sameStructureAs = function (other) {
+  if (Array.isArray(this) !== Array.isArray(other)) { return false };
   for (var i = 0; i < this.length; i++) {
     if ((Array.isArray(this[i])) && (Array.isArray(other[i]))) {
-      console.log(`Level ${x}: both are arrays`)
-      x++;
-      this[i].sameStructureAs(other[i]);
+      if (this[i].length !== other[i].length) { return false };
+      return this[i].sameStructureAs(other[i]);
     } else if ((Array.isArray(this[i])) !== (Array.isArray(other[i]))) {
-      console.log(`Level ${x}: one is array`)
       return false;
     } else {
-      console.log(`Level ${x}: none are arrays`)
       continue;
     };
   };
   return true;
 };
-
-console.log([ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] ),
-           ![ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] )); // false
-
-// console.log([ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ),
-//             [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ) === true);
-
-// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ),
-//             [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ) === true);
-//
-// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] ),
-//            ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] )); // false
-//
-// console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] ),
-//            ![ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] )); // false
-//
-// console.log([ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ),
-//             [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] )); // true
