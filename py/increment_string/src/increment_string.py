2d682cd0edeb8ffb29bdb6a4dc9cade29f65dd17
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-13 12:58:06 +0100

    Sample tests passed

--- py/increment_string/src/increment_string.py
+++ py/increment_string/src/increment_string.py
@@ -1,2 +1,21 @@
 def increment_string(string):
-    return string + '1'
+    if string == '': return '1'
+    if not string[-1].isdigit(): return string + '1'
+
+    num_string = ''
+    i = 0
+    for x in reversed(string):
+        if i < 0 and x == '0':
+            string = string[:i]
+            break
+
+        if x.isdigit():
+            num_string = x + num_string
+            i -= 1
+        else:
+            string = string[:i]
+            break
+
+    if string[-1] == '0' and num_string.replace('9', '') == '':
+        string = string[:-1]
+    return string + str(int(num_string) + 1)
--- py/increment_string/tests/test_increment_string.py
+++ py/increment_string/tests/test_increment_string.py
@@ -1,6 +1,3 @@
-# !/usr/bin/env python
-# -*- coding: utf8 -*-
-
 import unittest
 
 from src.increment_string import increment_string
@@ -9,4 +6,28 @@ class TestIncrementString(unittest.TestCase):
     def test_string_without_numbers_add_one(self):
         self.assertEqual(increment_string('foo'), 'foo1')
 
-    
+    def test_string_with_one_digit_number_increments_properly(self):
+        self.assertEqual(increment_string('bar1'), 'bar2')
+        self.assertEqual(increment_string('foobar3'), 'foobar4')
+
+    def test_string_with_two_digit_number_increments_properly(self):
+        self.assertEqual(increment_string('hello world12'), 'hello world13')
+        self.assertEqual(increment_string('hello world19'), 'hello world20')
+
+    def test_string_with_three_digit_number_increments_properly(self):
+        self.assertEqual(increment_string('hello world123'), 'hello world124')
+        self.assertEqual(increment_string('hello world999'), 'hello world1000')
+
+    def test_numbers_in_middle_do_not_confuse_function(self):
+        self.assertEqual(increment_string('hell0 world999'), 'hell0 world1000')
+
+    def test_numbers_beginning_with_zeroes(self):
+        self.assertEqual(increment_string('hell0 world01'), 'hell0 world02')
+        self.assertEqual(increment_string('hell0 world099'), 'hell0 world100')
+
+    def test_string_ending_with_multiple_zeroes(self):
+        self.assertEqual(increment_string('hell0 world00'), 'hell0 world01')
+        self.assertEqual(increment_string('hell0 world000'), 'hell0 world001')
+
+    def test_works_with_empty_strings(self):
+        self.assertEqual(increment_string(''), '1')
