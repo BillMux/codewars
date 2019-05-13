b06e1456eade7b8d94729048f6794f57411fa0fb
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-13 17:54:36 +0100

    Complete challenge, return formatted list

--- py/name_list/src/name_list.py
+++ py/name_list/src/name_list.py
@@ -1,2 +1,19 @@
 def name_list(names):
-    return names
+    # why would you even use a hash th0?
+
+    list = []
+    string = ''
+    for hsh in names:
+        list.append(hsh['name'])
+
+
+    for index, name in enumerate(list):
+        names_left = len(list) - index
+        if names_left > 2:
+            string += f"{name}, "
+        elif names_left == 2:
+            string += f"{name} & "
+        elif names_left == 1:
+            string += name
+        else: return ''
+    return string
--- py/name_list/tests/test_name_list.py
+++ py/name_list/tests/test_name_list.py
@@ -3,5 +3,34 @@ import unittest
 from src.name_list import name_list
 
 class TestNameList(unittest.TestCase):
-    def test_connection(self):
-        self.assertEqual(name_list(True), True)
+    def test_empty_input(self):
+        self.assertEqual(name_list([]), '')
+
+    def test_single_length_hash(self):
+        self.assertEqual(name_list([{'name': 'Bart'}]), 'Bart')
+
+    def test_hash_length_two(self):
+        self.assertEqual(name_list(
+        [
+            {'name': 'Bart'}, {'name': 'Lisa'}
+        ]),
+        'Bart & Lisa')
+
+    def test_length_three(self):
+        self.assertEqual(name_list(
+        [
+            {'name': 'Homer'}, {'name': 'Bart'}, {'name': 'Lisa'}
+        ]),
+        'Homer, Bart & Lisa')
+
+    def test_multiple_names(self):
+        self.assertEqual(name_list(
+        [
+            {'name': 'Abe'},
+            {'name': 'Homer'},
+            {'name': 'Marge'},
+            {'name': 'Bart'},
+            {'name': 'Lisa'},
+            {'name': 'Maggie'}
+        ]),
+        'Abe, Homer, Marge, Bart, Lisa & Maggie')
