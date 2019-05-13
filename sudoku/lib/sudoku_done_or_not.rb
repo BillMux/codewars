0534a777df176091fa03c9133689a379dcb4fb29
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-13 11:33:58 +0100

    Complete sudoku ruby challenge with tdd

---
+++ sudoku/lib/sudoku_done_or_not.rb
@@ -0,0 +1,26 @@
+def done_or_not(grid)
+  flip = grid.transpose
+  return 'Try again!' if repeat?(grid) || repeat?(flip) || repeat?(squares(grid))
+  'Finished!'
+end
+
+def repeat?(grid)
+  grid.each { |arr| return true if arr.uniq != arr }
+  false
+end
+
+def squares(grid)
+  squares, i, j = [], 0, 0
+  while i < grid.length
+    while j < 9
+      squares << [
+        grid[i][j], grid[i][j + 1], grid[i][j + 2],
+        grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2],
+        grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2],
+      ]
+      j += 3
+    end
+    i += 3
+  end
+  squares
+end
