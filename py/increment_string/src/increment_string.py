ee7779ffd237738cff41a29c27789147836dec41
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-13 16:37:19 +0100

    Refactor using .rstrip and z.fill

--- py/increment_string/src/increment_string.py
+++ py/increment_string/src/increment_string.py
@@ -1,19 +1,5 @@
 def increment_string(string):
-    if string == '' or not string[-1].isdigit(): return string + '1'
-    elif len(string) == 1: return str(int(string) + 1)
-
-    num_string = ''
-    for i, x in enumerate(reversed(string)):
-        if i > 0 and x == '0':
-            string = string[:-i]
-            break
-
-        if x.isdigit() and i != len(string) - 1:
-            num_string = x + num_string
-        elif i > 0:
-            string = string[:-i]
-            break
-
-    if string[-1] == '0' and num_string.replace('9', '') == '':
-        string = string[:-1]
-    return string + str(int(num_string) + 1)
+    before = string.rstrip('1234567890')
+    after = string[len(before):]
+    if after == '': return before + '1'
+    return before + str(int(after) + 1).zfill(len(after))
