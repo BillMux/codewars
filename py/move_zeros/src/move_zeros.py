03a26185497d7c484b475b779b847c644c14814c
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-20 14:31:22 +0100

    Works with lists of numbers and string

--- py/move_zeros/src/move_zeros.py
+++ py/move_zeros/src/move_zeros.py
@@ -1,6 +1,8 @@
 def move_zeros(numbers):
     i = 0
     while i < len(numbers) - numbers.count(0):
-        if numbers[i] == 0: numbers.insert((len(numbers) - 1), numbers.pop(i))
+        if numbers[i] == 0 and str(numbers[i]) != 'False':
+            numbers.insert((len(numbers) - 1), numbers.pop(i))
         else: i += 1
+    print(str(numbers))
     return numbers
--- py/move_zeros/tests/test_move_zeros.py
+++ py/move_zeros/tests/test_move_zeros.py
@@ -13,11 +13,19 @@ class TestMoveZeros(unittest.TestCase):
         )
 
     def test_long_list(self):
-        self.assertEqual(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),
-        [9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+        self.assertEqual(
+                move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),
+                [9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]
+            )
 
     def test_list_with_strings(self):
-        self.assertEqual(move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),
-        ["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+        self.assertEqual(
+            move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),
+            ["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]
+        )
 
-    
+    def test_with_other_objects(self):
+        self.assertEqual(
+            move_zeros(["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]),
+            ["a","b",None,"c","d",1,False,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0]
+        )
