25c504c35beb6a47e7fcdd2292566c3efed9f0e0
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-06-18 08:39:59 +0100

    Complete creadit card validation

--- py/validate/src/validate.py
+++ py/validate/src/validate.py
@@ -1,11 +1,7 @@
 # 1. Double every other digit, starting from the end of the number
-# (if there are an even number of digits double every other digit
-# starting from the first, if there are an odd number, start from the second)
 
 # 2. If one of these numbers becomes a two digit number, replace it with the sum
 # of its digits
-# (this can be done by subtracting 9, as the resulting number cannot be greater
-# than 18)
 
 # 3. Sum the final digits
 
@@ -14,6 +10,13 @@
 def validate(number):
     count = 0
     for index, digit in enumerate(reversed([int(x) for x in str(number)])):
-        count += digit if index % 2 == 0 else digit * 2 - 9
-        if index % 2 == 1 and digit > 4: count -= 9
+        count += digit if index % 2 == 0 else digit * 2
+        if index % 2 == 1 and digit > 5: count -= 9
     return count % 10 == 0
+
+    # alternative solution
+    # while index < len(digit_list):
+    #     digit_list[index] *= 2
+    #     if digit_list[index] > 9: digit_list[index] -= 9
+    #     index += 2
+    # return sum(digit_list) % 10 == 0
--- py/validate/tests/test_validate.py
+++ py/validate/tests/test_validate.py
@@ -16,12 +16,3 @@ class TestValidate(unittest.TestCase):
 
     def test_longer_numbers_with_high_digits(self):
         assert validate(8675309) == False
-
-
-
-
-# 1714 ==> [1*, 7, 1*, 4] ==> [2, 7, 2, 4]
-#
-# 12345 ==> [1, 2*, 3, 4*, 5] ==> [1, 4, 3, 8, 5]
-#
-# 891 ==> [8, 9*, 1] ==> [8, 18, 1]
