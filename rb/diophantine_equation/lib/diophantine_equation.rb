7b7804c50557c4f08139c5cfdb7ac2c17a08cc97
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-06-25 16:35:55 +0100

    Start diophantine equation challenge

--- rb/diophantine_equation/lib/diophantine_equation.rb
+++ rb/diophantine_equation/lib/diophantine_equation.rb
@@ -1,3 +1,9 @@
+# x2 - 4 * y2 = (x - 2*y) * (x + 2*y)
+
+# Return an array containing arrays with two numbers
+# that can be placed in this equation in the positions
+# of x and y
+
 def diophantine
   return 'hello world'
 end
