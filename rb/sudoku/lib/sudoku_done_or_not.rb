68c3eae84d152891fe5ae586fe88b353dd48b971
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-07-04 12:12:33 +0100

    Add get_squares helper method

--- rb/sudoku/lib/sudoku_done_or_not.rb
+++ rb/sudoku/lib/sudoku_done_or_not.rb
@@ -3,23 +3,28 @@ def sudoku_is_valid(grid)
 end
 
 def valid?(grid)
-  grid.each { |arr| return false if arr.uniq != arr || arr.sum != 45 }
+  grid.each do |arr|
+    return false if arr.uniq != arr || arr.sum != 45 || arr.any? { |x| x > 9 }
+  end
   true
 end
 
 def squares(grid)
   squares, i, j = [], 0, 0
-  grid.map { |row| p row.each_slice(3).to_a }
   while i < grid.length
     while j < grid[0].length
-      squares << [
-        grid[i][j], grid[i][j + 1], grid[i][j + 2],
-        grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2],
-        grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]
-      ]
+      squares << get_squares(grid, i, j)
       j += 3
     end
     i += 3
   end
   squares
+end
+
+def get_squares(grid, i, j)
+  [
+    grid[i][j], grid[i][j + 1], grid[i][j + 2],
+    grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2],
+    grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]
+  ]
 end
--- rb/sudoku/spec/sudoku_done_or_not_spec.rb
+++ rb/sudoku/spec/sudoku_done_or_not_spec.rb
@@ -8,6 +8,11 @@ describe '#valid?' do
   it 'returns true if row contains unique integer values from one to nine' do
     expect(valid?([[1, 2, 3, 4, 5, 6, 7, 8, 9]])).to be true
   end
+
+  it 'returns false if any numbers over 9' do
+    expect(valid?([[5, 3, 1, 6, 7, 4, 9, 11, 2]])).to be false
+    expect(valid?([[0, 2, 3, 4, 5, 6, 7, 8, 10]])).to be false
+  end
 end
 
 describe '#sudoku_is_valid' do
@@ -16,10 +21,12 @@ describe '#sudoku_is_valid' do
   end
 
   it 'returns false!" if numbers repeat in column' do
-    expect(sudoku_is_valid([
-      [5, 3, 4, 6, 7, 4, 9, 1, 2],
-      [6, 3, 2, 1, 9, 5, 3, 4, 8]
-    ])).to be false
+    expect(sudoku_is_valid(
+      [
+        [5, 3, 4, 6, 7, 4, 9, 1, 2],
+        [6, 3, 2, 1, 9, 5, 3, 4, 8]
+      ]
+    )).to be false
   end
 
   it 'returns false if incomplete 3x3 squares' do
@@ -50,9 +57,5 @@ describe '#sudoku_is_valid' do
         [3, 4, 5, 2, 8, 6, 1, 7, 9]
       ]
     )).to eq true
-  end
-
-  it 'returns false if any numbers over 9' do
-    expect(sudoku_is_valid([[5, 3, 1, 6, 7, 4, 9, 11, 2]])).to be false
   end
 end
