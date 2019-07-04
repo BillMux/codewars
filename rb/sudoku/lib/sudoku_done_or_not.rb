e9b663d59e1de0a1ded011ed0d5f091f991cc430
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-07-04 11:54:56 +0100

    Testing for numbers over 9 edge case

--- rb/sudoku/lib/sudoku_done_or_not.rb
+++ rb/sudoku/lib/sudoku_done_or_not.rb
@@ -1,22 +1,21 @@
-def done_or_not(grid)
-  flip = grid.transpose
-  return 'Try again!' if repeat?(grid) || repeat?(flip) || repeat?(squares(grid))
-  'Finished!'
+def sudoku_is_valid(grid)
+  valid?(grid) && valid?(grid.transpose) && valid?(squares(grid))
 end
 
-def repeat?(grid)
-  grid.each { |arr| return true if arr.uniq != arr }
-  false
+def valid?(grid)
+  grid.each { |arr| return false if arr.uniq != arr || arr.sum != 45 }
+  true
 end
 
 def squares(grid)
   squares, i, j = [], 0, 0
+  grid.map { |row| p row.each_slice(3).to_a }
   while i < grid.length
     while j < grid[0].length
       squares << [
         grid[i][j], grid[i][j + 1], grid[i][j + 2],
         grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2],
-        grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2],
+        grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]
       ]
       j += 3
     end
--- rb/sudoku/spec/sudoku_done_or_not_spec.rb
+++ rb/sudoku/spec/sudoku_done_or_not_spec.rb
@@ -1,57 +1,58 @@
 require_relative '../lib/sudoku_done_or_not.rb'
 
-describe '#repeat?' do
-  it 'returns true if row contains duplicate' do
-    expect(repeat?([[1, 1]])).to be true
+describe '#valid?' do
+  it 'returns false if row contains duplicate' do
+    expect(valid?([[1, 1]])).to be false
   end
 
-  it 'return false if row contains unique values' do
-    expect(repeat?([[1, 2]])).to be false
+  it 'returns true if row contains unique values' do
+    expect(valid?([[1, 2]])).to be true
   end
 end
 
-describe '#done_or_not' do
-  it 'returns "Try again!" if numbers repeat in row' do
-    expect(done_or_not([[5, 3, 4, 6, 7, 4, 9, 1, 2]]
-    )).to eq 'Try again!'
+describe '#sudoku_is_valid' do
+  it 'returns false if numbers repeat in row' do
+    expect(sudoku_is_valid([[5, 3, 4, 6, 7, 4, 9, 1, 2]])).to be false
   end
 
-  it 'returns "Try again!" if numbers repeat in row' do
-    expect(done_or_not([
+  it 'returns false!" if numbers repeat in column' do
+    expect(sudoku_is_valid([
       [5, 3, 4, 6, 7, 4, 9, 1, 2],
       [6, 3, 2, 1, 9, 5, 3, 4, 8]
-    ])).to eq 'Try again!'
+    ])).to be false
   end
 
-  it 'returns "Try again!" if incomplete 3x3 squares' do
-    expect(done_or_not(
+  it 'returns false if incomplete 3x3 squares' do
+    expect(sudoku_is_valid([
+      [5, 4, 4, 6, 7, 8, 9, 1, 2],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [3, 2, 6, 8, 5, 4, 7, 9, 1],
+      [7, 1, 4, 9, 2, 3, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [4, 3, 5, 2, 8, 6, 1, 7, 9]
+    ])).to be false
+  end
+
+  it 'returns true if complete' do
+    expect(sudoku_is_valid(
       [
-        [5, 4, 4, 6, 7, 8, 9, 1, 2],
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
         [6, 7, 2, 1, 9, 5, 3, 4, 8],
         [1, 9, 8, 3, 4, 2, 5, 6, 7],
         [8, 5, 9, 7, 6, 1, 4, 2, 3],
-        [3, 2, 6, 8, 5, 4, 7, 9, 1],
-        [7, 1, 4, 9, 2, 3, 8, 5, 6],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-        [4, 3, 5, 2, 8, 6, 1, 7, 9]
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
       ]
-    )).to eq 'Try again!'
+    )).to eq true
   end
 
-  # it 'returns "Finished!" if complete' do
-  #   expect(done_or_not(
-  #     [
-  #       [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  #       [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  #       [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  #       [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  #       [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  #       [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  #       [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  #       [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  #       [3, 4, 5, 2, 8, 6, 1, 7, 9]
-  #     ]
-  #   )).to eq 'Finished!'
-  # end
+  it 'returns false if any numbers over 9' do
+    expect(sudoku_is_valid([[5, 3, 1, 6, 7, 4, 9, 11, 2]])).to be false
+  end
 end
