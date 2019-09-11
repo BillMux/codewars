a7778028024404cef529bea9b9f064ba3601189b
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-09-11 17:34:29 +0100

    'Dont Give Me Five' kata completer

---
+++ c#/DontGiveMeFive/DontGiveMeFive/DontGiveMeFive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DontGiveMeFive
+{
+    public class Kata
+    {
+        static void Main() {}
+
+        public static int LoseFives(int start, int end)
+        {
+            int output = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (!("" + i).Contains("5")) output++;
+            }
+            return output;
+        }
+    }
+}
---
+++ c#/DontGiveMeFive/DontGiveMeFiveTests/DontGiveMeFiveTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using DontGiveMeFive;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestWithoutFives()
+        {
+            int result = Kata.LoseFives(0, 4);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void TestWithFive()
+        {
+            int result = Kata.LoseFives(0, 5);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void TestHigherNumbers()
+        {
+            int result = Kata.LoseFives(4, 17);
+            Assert.AreEqual(12, result);
+        }
+    }
+}
