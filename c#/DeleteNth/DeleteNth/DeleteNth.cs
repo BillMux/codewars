9d355c72ac5157f94849c37507b62ad30198ae3a
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-09-14 20:21:39 +0100

    First successful test run

---
+++ c#/DeleteNth/DeleteNth/DeleteNth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Kata
+{
+    public class DeleteNth
+    {
+        static void Main() {}
+
+        public int[] Run(int[] numbers, int limit)
+        {
+            List<int> numbersList = new List<int>(numbers);
+            Hashtable countTable = new Hashtable();
+
+            for (int i = 0; i < numbersList.Count; i++)
+            {
+                int number = numbersList[i];
+                if (countTable.Contains(number))
+                {
+                    countTable[number] = (int)countTable[number] + 1;
+                }
+                else
+                {
+                    countTable.Add(number, 1);
+                }
+                if ((int)countTable[number] > limit)
+                {
+                    numbersList.RemoveAt(i);
+                }
+            }
+            return numbersList.ToArray();
+        }
+
+    }
+}
---
+++ c#/DeleteNth/DeleteNthTests/DeleteNthTest.cs
@@ -0,0 +1,36 @@
+using Kata;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        DeleteNth test = new DeleteNth();
+
+        [Test]
+        public void TestListNotExceedingLimitReturnsSameList()
+        {
+            int[] input = { 1, 2, 3 };
+            int[] expect = test.Run(input, 2);
+            Assert.AreEqual(expect, input);
+        }
+
+        [Test]
+        public void TestListOneNumberExceedsLimit()
+        {
+            int[] input = { 1, 2, 3, 3, 3 };
+            int[] expect = { 1, 2, 3, 3 };
+            int[] result = test.Run(input, 2);
+            Assert.AreEqual(expect, result);
+        }
+
+        [Test]
+        public void RandomTest1()
+        {
+            int[] input = { 1, 1, 3, 3, 7, 2, 2, 2, 2 };
+            int[] expect = { 1, 1, 3, 3, 7, 2, 2, 2 };
+            int[] result = test.Run(input, 3);
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
